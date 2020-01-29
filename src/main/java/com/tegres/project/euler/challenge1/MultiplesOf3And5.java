800831e62d2119dbb832ebf3fe96a1d7ece1bdb3
EdwinGertse <edwin.gertse@gmail.com>
2020-01-29 23:46:48 +0200

    Add problem description

--- src/main/java/com/tegres/project/euler/challenge1/MultiplesOf3And5.java
+++ src/main/java/com/tegres/project/euler/challenge1/MultiplesOf3And5.java
@@ -5,6 +5,16 @@ import com.tegres.project.euler.common.Multiples;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Multiples of 3 and 5
+ *
+ * Problem 1
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ * @author Edwin Gertse
+ */
 public class MultiplesOf3And5 {
 
     private Integer isMultipleOf(Integer number) {
