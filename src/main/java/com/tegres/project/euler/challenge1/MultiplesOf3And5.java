d5bb53936f2388e386e8fa4ab8a14601cf28381c
EdwinGertse <edwin.gertse@gmail.com>
2020-02-07 00:48:06 +0200

    Refactor problem 1 - implement Optional

--- src/main/java/com/tegres/project/euler/challenge1/MultiplesOf3And5.java
+++ src/main/java/com/tegres/project/euler/challenge1/MultiplesOf3And5.java
@@ -4,6 +4,7 @@ import com.tegres.project.euler.common.Multiples;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Multiples of 3 and 5
@@ -17,16 +18,16 @@ import java.util.List;
  */
 public class MultiplesOf3And5 {
 
-    private Integer isMultipleOf(Integer number) {
-        if (Multiples.MULITPLES_OF_THREE.test(number)) return number;
-        else if (Multiples.MULITPLES_OF_FIVE.test(number)) return number;
-        else return null;
+    private Optional<Integer> isMultipleOf(Integer number) {
+        if (Multiples.MULITPLES_OF_THREE.test(number)) return Optional.of(number);
+        else if (Multiples.MULITPLES_OF_FIVE.test(number)) return Optional.of(number);
+        else return Optional.empty();
     }
 
     public Integer sumOfMultiples(Integer maxNumber) {
         List<Integer> multiples = new ArrayList<>();
         for(int i = 0; i < maxNumber; i++) {
-            if (isMultipleOf(i) != null) multiples.add(i);
+            if (isMultipleOf(i).isPresent()) multiples.add(i);
         }
         return multiples.stream().mapToInt(Integer::intValue).sum();
     }
