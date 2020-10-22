663be9cf5a09043561014494be9fcdd0f3c96a05
EdwinGertse <edwin.gertse@gmail.com>
2020-10-22 18:41:58 +0200

    Refactor challenge

--- src/main/java/com/tegres/project/euler/challenge9/Labels.java
+++
@@ -1,7 +0,0 @@
-package com.tegres.project.euler.challenge9;
-
-public class Labels {
-    public static final String A = "a";
-    public static final String B = "b";
-    public static final String C = "c";
-}
